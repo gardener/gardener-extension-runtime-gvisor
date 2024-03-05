5f666b79dbe0f8d77c6820c71709b5d5ce9e78f1
Shafeeque E S <shafeeque.e.s@sap.com>
2024-03-05 11:20:52 +0530

    Drop support for Shoots with Kubernetes version == 1.24 (#110)
    * Adapt PDBs to use `unhealthyPodEvictionPolicy: AlwaysAllow`
    
    * Drop PSP deployment
    
    * Drop usages of 1.24
    
    * Run `make generate`

--- pkg/controller/actuator_reconcile.go
+++ pkg/controller/actuator_reconcile.go
@@ -19,7 +19,6 @@ import (
 	"fmt"
 
 	extensionscontroller "github.com/gardener/gardener/extensions/pkg/controller"
-	v1beta1helper "github.com/gardener/gardener/pkg/apis/core/v1beta1/helper"
 	extensionsv1alpha1 "github.com/gardener/gardener/pkg/apis/extensions/v1alpha1"
 	"github.com/gardener/gardener/pkg/utils/managedresources"
 	"github.com/go-logr/logr"
@@ -43,8 +42,7 @@ func (a *actuator) Reconcile(ctx context.Context, log logr.Logger, cr *extension
 
 	log.Info("Preparing gVisor installation", "shoot", cluster.Shoot.Name, "shootNamespace", cluster.Shoot.Namespace)
 	// create MR containing the prerequisites for the installation DaemonSet
-	pspDisabled := v1beta1helper.IsPSPDisabled(cluster.Shoot)
-	gVisorChart, err := charts.RenderGVisorChart(chartRenderer, pspDisabled)
+	gVisorChart, err := charts.RenderGVisorChart(chartRenderer)
 	if err != nil {
 		return err
 	}
