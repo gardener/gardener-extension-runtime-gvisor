712642418a5d941dfeed8deffd891315e1128fa9
Marc Vornetran <marc1404@users.noreply.github.com>
2026-08-26 18:19:27 +0200

    Update copyright text (#460)
    * Update copyright text
    
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>
    
    * Fix copyright text: remove redundant "Copyright" prefix
    
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>
    
    * Fix copyright text: remove redundant "Copyright" prefix
    
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>
    
    ---------
    
    Signed-off-by: Marc Vornetran <marc.vornetran@sap.com>

--- hack/tools.go
+++ hack/tools.go
@@ -2,7 +2,7 @@
 //go:build tools
 // +build tools
 
-// SPDX-FileCopyrightText: 2024 SAP SE or an SAP affiliate company and Gardener contributors
+// SPDX-FileCopyrightText: Contributors to the Gardener project
 //
 // SPDX-License-Identifier: Apache-2.0
 
